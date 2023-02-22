8e6a154591729ce359afba2e51f3b2f8b05148bc
Veronika <veronikagovoruha@gmail.com>
2023-02-22 21:42:38 +0200

    add slider to stories section

--- index.js
+++ index.js
@@ -115,4 +115,41 @@ $(function(){
           }
         ]
       });
+
+      $('.slider__list-stories').slick({
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        prevArrow: ('.stories__btn-right'),
+        nextArrow: ('.stories__btn-left'),
+        responsive: [
+          {
+            breakpoint: 1336,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots:true,
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              dots: true,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 })
